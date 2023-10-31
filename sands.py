6156596fafd96b5d14a39b0205e98bfe5c4bb713
hyunholee <dionme2622@gmail.com>
2023-11-01 02:12:52 +0900

    Sands, Pinkbean, Gray 클래스 생성 완료

---
+++ sands.py
@@ -0,0 +1,20 @@
+class Get_Ball:
+
+    @staticmethod
+    def enter(character, e):
+        pass
+
+    @staticmethod
+    def exit(character, e):
+        pass
+
+    @staticmethod
+    def do(character):
+        pass
+
+    @staticmethod
+    def draw(character):
+        pass
+
+    class Sands:
+        pass
