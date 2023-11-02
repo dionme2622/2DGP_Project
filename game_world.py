288ee4e2c1789d54caf355c9921297cccfd37396
hyunholee <dionme2622@gmail.com>
2023-11-02 15:49:31 +0900

    world를 objects로 변경

--- game_world.py
+++ game_world.py
@@ -1,28 +1,33 @@
 # 게임 월드 모듈
 
 # 게임 월드 표현
-world = [ [], [], [] ]         # 게임 월드는 리스트로 표현
+objects = [[] for _ in range(4)]         # 게임 월드는 리스트로 표현
 
 # 게임 월드에 객체 담기
 def add_object(o, depth = 0):
-    world[depth].append(o)      # 지정된 깊이의 레이어에 객체 추가
+    objects[depth].append(o)      # 지정된 깊이의 레이어에 객체 추가
+
+
+def add_objects(ol, depth = 0):
+    objects[depth] += ol
+
 
 # 게임 월드 객체들을 모두 다 업데이트
 def update():
-    for layer in world:
+    for layer in objects:
         for o in layer:
             o.update()
 
 
 # 게임 월드의 객체들을 몽땅 그리기
 def render():
-    for layer in world:
+    for layer in objects:
         for o in layer:
             o.draw()
 
 # 객체 삭제
 def remove_object(o):
-    for layer in world:
+    for layer in objects:
         if o in layer:
             layer.remove(o)
             return
