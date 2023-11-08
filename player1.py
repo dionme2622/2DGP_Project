fcb0731b645d5c74a87eb3683ab0247dd04874ba
hyunholee <dionme2622@gmail.com>
2023-11-08 18:36:28 +0900

    player1,2 이동 구현 완료

--- player1.py
+++ player1.py
@@ -1,40 +1,39 @@
 from pico2d import load_image
 
 import game_framework
-from sdl2 import SDL_KEYDOWN, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT, SDLK_UP, SDLK_DOWN
+from sdl2 import SDL_KEYDOWN, SDL_KEYUP, SDLK_r, SDLK_d, SDLK_f, SDLK_g
 
 
-
-def right_down(e):
-    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
-
-
-def right_up(e):
-    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+def r_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_r
 
 
-def left_down(e):
-    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+def r_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_r
 
 
-def left_up(e):
-    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
+def d_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_d
 
 
-def up_down(e):
-    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_UP
+def d_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_d
 
 
-def up_up(e):
-    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_UP
+def f_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_f
 
 
-def down_down(e):
-    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_DOWN
+def f_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_f
 
 
-def down_up(e):
-    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_DOWN
+def g_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_g
+
+
+def g_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_g
 
 
 def lets_idle(e):
@@ -71,7 +70,7 @@ class Idle:
 
     @staticmethod
     def draw(character):
-        character.image.clip_composite_draw(int(character.frame) * 250, character.action * 420, 250, 330, 0, 'h',
+        character.image.clip_draw(int(character.frame) * 250, character.action * 420, 250, 330,
                                             character.x, character.y, 100, 150)
 
 
@@ -79,20 +78,20 @@ class Run:
 
     @staticmethod
     def enter(character, e):
-        if right_down(e):
+        if g_down(e):
             character.dir_right = 1
             character.dirX, character.action = 1, 1
-        elif left_down(e):
+        elif d_down(e):
             character.dir_left = 1
             character.dirX, character.action = -1, 1
-        elif up_down(e):
+        elif r_down(e):
             character.dir_up = 1
             character.dirY, character.action = 1, 1
-        elif down_down(e):
+        elif f_down(e):
             character.dir_down = 1
             character.dirY, character.action = -1, 1
 
-        elif right_up(e):
+        elif g_up(e):
             character.dir_right = 0
             character.dirX = 0
 
@@ -102,7 +101,7 @@ class Run:
                 character.dirX, character.dirY, character.action = 0, 1, 1
             elif character.dir_down == 1:
                 character.dirX, character.dirY, character.action = 0, -1, 1
-        elif left_up(e):
+        elif d_up(e):
             character.dir_left = 0
             character.dirX = 0
             if character.dir_right == 1:
@@ -111,7 +110,7 @@ class Run:
                 character.dirX, character.dirY, character.action = 0, 1, 1
             elif character.dir_down == 1:
                 character.dirX, character.dirY, character.action = 0, -1, 1
-        elif up_up(e):
+        elif r_up(e):
             character.dir_up = 0
             character.dirY = 0
             if character.dir_down == 1:
@@ -121,7 +120,7 @@ class Run:
             elif character.dir_left == 1:
                 character.dirX, character.dirY, character.action = -1, 0, 1
 
-        elif down_up(e):
+        elif f_up(e):
             character.dir_down = 0
             character.dirY = 0
             if character.dir_up == 1:
@@ -141,13 +140,16 @@ class Run:
     @staticmethod
     def do(character):
         character.frame = (character.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
+        if character.x <= 620 + 40: character.x = 620 + 40
+        elif character.x >= 1280 - 30: character.x = 1280 - 30
         character.x += character.dirX * RUN_SPEED_PPS * game_framework.frame_time
+        if character.y >= 800: character.y = 800
+        elif character.y <= 80: character.y = 80
         character.y += character.dirY * RUN_SPEED_PPS * game_framework.frame_time
 
     @staticmethod
     def draw(character):
-        character.image.clip_composite_draw(int(character.frame) * 250, character.action * 420, 250, 330, 0, 'h',
-                                            character.x, character.y, 100, 150)
+        character.image.clip_draw(int(character.frame) * 250, character.action * 420, 250, 330, character.x, character.y, 100, 150)
 
 
 class StateMachine:
@@ -155,10 +157,10 @@ class StateMachine:
         self.character = character
         self.cur_state = Idle
         self.transitions = {
-            Idle: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, up_down: Run, up_up: Run,
-                   down_down: Run, down_up: Run},
-            Run: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, up_down: Run,
-                  up_up: Run, down_down: Run, down_up: Run, lets_idle: Idle}
+            Idle: {g_down: Run, d_down: Run, g_up: Run, d_up: Run, r_down: Run, r_up: Run,
+                   f_down: Run, f_up: Run},
+            Run: {g_down: Run, d_down: Run, g_up: Run, d_up: Run, r_down: Run,
+                  r_up: Run, f_down: Run, f_up: Run, lets_idle: Idle}
         }
 
     def start(self):
@@ -182,13 +184,13 @@ class StateMachine:
 
 class Player1:
     def __init__(self, ch):
-        self.x, self.y = 400, 500
+        self.x, self.y = 50, 500
         self.hp, self.mp, self.speed, self.attack_speed = ch.hp, ch.mp, ch.speed, ch.attack_speed
         self.frame = ch.frame
         self.action = ch.action  # 오른쪽 IDLE
         self.dirX = ch.dirX
         self.dirY = ch.dirY
-        self.face_dir = 1  # 오른쪽 방향 얼굴을 향하고 있음
+        self.face_dir = -1  # 왼쪽 방향 얼굴을 향하고 있음
         self.image = ch.image
         self.dir_left, self.dir_right, self.dir_up, self.dir_down = ch.dir_left, ch.dir_right, ch.dir_up, ch.dir_down
         self.state_machine = StateMachine(self)
@@ -201,46 +203,3 @@ class Player1:
 
     def draw(self):
         self.state_machine.draw()
-
-
-# class Pinkbean:
-#     def __init__(self):
-#         self.x, self.y = 800, 500
-#         self.frame = 0
-#         self.action = 3  # 오른쪽 IDLE
-#         self.dir_x = 0
-#         self.dir_y = 0
-#         self.face_dir = 1  # 오른쪽 방향 얼굴을 향하고 있음
-#         self.image = load_image('sands.png')
-#         self.state_machine = StateMachine(self)
-#         self.state_machine.start()
-#
-#     def update(self):
-#         self.state_machine.update()
-#
-#     def handle_event(self, event):
-#         self.state_machine.handle_event(('INPUT', event))
-#
-#     def draw(self):
-#         self.state_machine.draw()
-#
-# class Gray:
-#     def __init__(self):
-#         self.x, self.y = 800, 500
-#         self.frame = 0
-#         self.action = 3  # 오른쪽 IDLE
-#         self.dir_x = 0
-#         self.dir_y = 0
-#         self.face_dir = 1  # 오른쪽 방향 얼굴을 향하고 있음
-#         self.image = load_image('sands.png')
-#         self.state_machine = StateMachine(self)
-#         self.state_machine.start()
-#
-#     def update(self):
-#         self.state_machine.update()
-#
-#     def handle_event(self, event):
-#         self.state_machine.handle_event(('INPUT', event))
-#
-#     def draw(self):
-#         self.state_machine.draw()
--- player2.py
+++ player2.py
@@ -1,14 +1,57 @@
 from pico2d import load_image
 
-import sands
+import game_framework
+from sdl2 import SDL_KEYDOWN, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT, SDLK_UP, SDLK_DOWN
+
+from sands import FRAMES_PER_ACTION, ACTION_PER_TIME, RUN_SPEED_PPS
+
+
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+
+
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+
+
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+
+
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
+
+
+def up_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_UP
+
+
+def up_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_UP
+
+
+def down_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_DOWN
+
+
+def down_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_DOWN
+
+
+def lets_idle(e):
+    return e[0] == 'LETS_IDLE'
+
+
+
+
+
 class Idle:
 
     @staticmethod
     def enter(character, e):
-        if character == Sands:
-            character.action = 3
-            character.dir_x = 0
-            character.frame = 0
+        character.dir_x = 0
+        character.frame = 0
+        character.action = 1
         pass
 
     @staticmethod
@@ -17,27 +60,104 @@ class Idle:
 
     @staticmethod
     def do(character):
-        character.frame = (character.frame + 1) % 4
+        pass
 
     @staticmethod
     def draw(character):
-        if character.__class__ == Sands:        # 해당 클래스가 샌즈이면 샌즈를 불러옴
-            print("샌즈")
-            character.image.clip_draw(character.frame * 250, 420, 250, 330, character.x, character.y, 100, 150)
-        elif character.__class__ == Pinkbean:
-            print("핑크빈")
-        elif character.__class__ == Gray:
-            print("그레이")
+        character.image.clip_composite_draw(int(character.frame) * 250, character.action * 420, 250, 330, 0, 'h',
+                                            character.x, character.y, 100, 150)
 
-        # move += 10
-        # Character.clip_draw(frame * 250, 420, 250, 330, x - 150 - move, y - 200, 100, 150)  # 이동 모션
+
+class Run:
+
+    @staticmethod
+    def enter(character, e):
+        if right_down(e):
+            character.dir_right = 1
+            character.dirX, character.action = 1, 1
+        elif left_down(e):
+            character.dir_left = 1
+            character.dirX, character.action = -1, 1
+        elif up_down(e):
+            character.dir_up = 1
+            character.dirY, character.action = 1, 1
+        elif down_down(e):
+            character.dir_down = 1
+            character.dirY, character.action = -1, 1
+
+        elif right_up(e):
+            character.dir_right = 0
+            character.dirX = 0
+
+            if character.dir_left == 1:
+                character.dirX, character.action = -1, 1
+            elif character.dir_up == 1:
+                character.dirX, character.dirY, character.action = 0, 1, 1
+            elif character.dir_down == 1:
+                character.dirX, character.dirY, character.action = 0, -1, 1
+        elif left_up(e):
+            character.dir_left = 0
+            character.dirX = 0
+            if character.dir_right == 1:
+                character.dirX, character.action = 1, 1
+            elif character.dir_up == 1:
+                character.dirX, character.dirY, character.action = 0, 1, 1
+            elif character.dir_down == 1:
+                character.dirX, character.dirY, character.action = 0, -1, 1
+        elif up_up(e):
+            character.dir_up = 0
+            character.dirY = 0
+            if character.dir_down == 1:
+                character.dirY, character.action = -1, 1
+            elif character.dir_right == 1:
+                character.dirX, character.dirY, character.action = 1, 0, 1
+            elif character.dir_left == 1:
+                character.dirX, character.dirY, character.action = -1, 0, 1
+
+        elif down_up(e):
+            character.dir_down = 0
+            character.dirY = 0
+            if character.dir_up == 1:
+                character.dirY, character.action = 1, 1
+            elif character.dir_right == 1:
+                character.dirX, character.dirY, character.action = 1, 0, 1
+            elif character.dir_left == 1:
+                character.dirX, character.dirY, character.action = -1, 0, 1
+
+        if character.dir_left == 0 and character.dir_right == 0 and character.dir_up == 0 and character.dir_down == 0:
+            character.state_machine.handle_event(('LETS_IDLE', 0))
+
+    @staticmethod
+    def exit(character, e):
+        pass
+
+    @staticmethod
+    def do(character):
+        character.frame = (character.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
+        if character.x >= 620: character.x = 620
+        elif character.x <= 50: character.x = 50
+        character.x += character.dirX * RUN_SPEED_PPS * game_framework.frame_time
+        if character.y >= 800: character.y = 800
+        elif character.y <= 80: character.y = 80
+        character.y += character.dirY * RUN_SPEED_PPS * game_framework.frame_time
+
+    @staticmethod
+    def draw(character):
+        character.image.clip_composite_draw(int(character.frame) * 250, character.action * 420, 250, 330, 0, 'h',
+                                            character.x, character.y, 100, 150)
+
+
 class StateMachine:
     def __init__(self, character):
         self.character = character
         self.cur_state = Idle
         self.transitions = {
-
+            Idle: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, up_down: Run, up_up: Run,
+                   down_down: Run, down_up: Run},
+            Run: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, up_down: Run,
+                  up_up: Run, down_down: Run, down_up: Run, lets_idle: Idle}
         }
+
     def start(self):
         self.cur_state.enter(self.character, ('NONE', 0))
 
@@ -57,66 +177,67 @@ class StateMachine:
     def draw(self):
         self.cur_state.draw(self.character)
 
-
-class Sands:
-    def __init__(self):
-        self.x, self.y = 800, 500
-        self.frame = 0
-        self.action = 3  # 오른쪽 IDLE
-        self.dir_x = 0
-        self.dir_y = 0
+class Player2:
+    def __init__(self, ch):
+        self.x, self.y = 1200, 500
+        self.hp, self.mp, self.speed, self.attack_speed = ch.hp, ch.mp, ch.speed, ch.attack_speed
+        self.frame = ch.frame
+        self.action = ch.action  # 오른쪽 IDLE
+        self.dirX = ch.dirX
+        self.dirY = ch.dirY
         self.face_dir = 1  # 오른쪽 방향 얼굴을 향하고 있음
-        self.image = load_image('sands.png')
+        self.image = ch.image
+        self.dir_left, self.dir_right, self.dir_up, self.dir_down = ch.dir_left, ch.dir_right, ch.dir_up, ch.dir_down
         self.state_machine = StateMachine(self)
         self.state_machine.start()
 
     def update(self):
         self.state_machine.update()
-
     def handle_event(self, event):
         self.state_machine.handle_event(('INPUT', event))
 
     def draw(self):
         self.state_machine.draw()
 
-class Pinkbean:
-    def __init__(self):
-        self.x, self.y = 800, 500
-        self.frame = 0
-        self.action = 3  # 오른쪽 IDLE
-        self.dir_x = 0
-        self.dir_y = 0
-        self.face_dir = 1  # 오른쪽 방향 얼굴을 향하고 있음
-        self.image = load_image('sands.png')
-        self.state_machine = StateMachine(self)
-        self.state_machine.start()
 
-    def update(self):
-        self.state_machine.update()
-
-    def handle_event(self, event):
-        self.state_machine.handle_event(('INPUT', event))
-
-    def draw(self):
-        self.state_machine.draw()
-
-class Gray:
-    def __init__(self):
-        self.x, self.y = 800, 500
-        self.frame = 0
-        self.action = 3  # 오른쪽 IDLE
-        self.dir_x = 0
-        self.dir_y = 0
-        self.face_dir = 1  # 오른쪽 방향 얼굴을 향하고 있음
-        self.image = load_image('sands.png')
-        self.state_machine = StateMachine(self)
-        self.state_machine.start()
-
-    def update(self):
-        self.state_machine.update()
-
-    def handle_event(self, event):
-        self.state_machine.handle_event(('INPUT', event))
-
-    def draw(self):
-        self.state_machine.draw()
+# class Pinkbean:
+#     def __init__(self):
+#         self.x, self.y = 800, 500
+#         self.frame = 0
+#         self.action = 3  # 오른쪽 IDLE
+#         self.dir_x = 0
+#         self.dir_y = 0
+#         self.face_dir = 1  # 오른쪽 방향 얼굴을 향하고 있음
+#         self.image = load_image('sands.png')
+#         self.state_machine = StateMachine(self)
+#         self.state_machine.start()
+#
+#     def update(self):
+#         self.state_machine.update()
+#
+#     def handle_event(self, event):
+#         self.state_machine.handle_event(('INPUT', event))
+#
+#     def draw(self):
+#         self.state_machine.draw()
+#
+# class Gray:
+#     def __init__(self):
+#         self.x, self.y = 800, 500
+#         self.frame = 0
+#         self.action = 3  # 오른쪽 IDLE
+#         self.dir_x = 0
+#         self.dir_y = 0
+#         self.face_dir = 1  # 오른쪽 방향 얼굴을 향하고 있음
+#         self.image = load_image('sands.png')
+#         self.state_machine = StateMachine(self)
+#         self.state_machine.start()
+#
+#     def update(self):
+#         self.state_machine.update()
+#
+#     def handle_event(self, event):
+#         self.state_machine.handle_event(('INPUT', event))
+#
+#     def draw(self):
+#         self.state_machine.draw()
