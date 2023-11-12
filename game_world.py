b70156d2f65a79c379c8656a86659dfcc34f2f85
hyunholee <dionme2622@gmail.com>
2023-11-12 16:54:19 +0900

    충돌 그룹 삭제 함수 생성

--- game_world.py
+++ game_world.py
@@ -55,6 +55,12 @@ def remove_object(o):
             return
     raise ValueError('왜 존재하지도 않는 걸 지우라구????')
 
+def remove_collision_object(o):
+    for pairs in collision_pairs.values():
+        if o in pairs[0]:
+            pairs[0].remove(o)
+        if o in pairs[1]:
+            pairs[1].remove(o)
 
 def clear():
     for layer in objects:
