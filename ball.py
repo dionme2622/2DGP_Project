921564574c2d90e408de98f8f944d32d6f716a73
hyunholee <dionme2622@gmail.com>
2023-11-14 01:47:20 +0900

    <ball> 화면 크기에 맞게 ball 위치 조정

--- ball.py
+++ ball.py
@@ -1,29 +1,33 @@
-from pico2d import load_image
+from pico2d import load_image, draw_rectangle
 
 import game_framework
 import game_world
 import play_mode
+from tkinter import *
 
+root = Tk()
+WIDTH, HEIGHT = root.winfo_screenwidth(), root.winfo_screenheight()
 
 class Ball:
     image = None
 
     def __init__(self, x = 400, y = 300, velocity = 1):
         if Ball.image == None:
-            Ball.image = load_image('ball41x41.png')
+            Ball.image = load_image('./object/ball41x41.png')
         self.x, self.y, self.velocity = x, y, velocity
 
     def get_bb(self):
-        return self.x - 10, self.y - 10, self.x + 10, self.y + 10
+        return self.x - 20, self.y - 20, self.x + 20, self.y + 20
 
     def draw(self):
         self.image.draw(self.x, self.y)
+        draw_rectangle(*self.get_bb())
 
     def update(self):
 
         if play_mode.player1.getball == True:
             self.velocity = play_mode.player1.attack_speed
-            self.x = play_mode.player1.x + 60
+            self.x = play_mode.player1.x + 80
             self.y = play_mode.player1.y
         else:
             self.x += self.velocity * 100 * game_framework.frame_time
@@ -31,15 +35,15 @@ class Ball:
         if play_mode.player2.getball == True:
             self.velocity = -play_mode.player2.attack_speed
 
-            self.x = play_mode.player2.x - 50
+            self.x = play_mode.player2.x - 80
             self.y = play_mode.player2.y
         else:
             self.x += self.velocity * 100 * game_framework.frame_time
 
-        if self.x > 1400:
+        if self.x > WIDTH + 50:
             play_mode.player2.getball = True
 
-        if self.x < -120 :
+        if self.x < 0 - 50 :
             play_mode.player1.getball = True
 
     def handle_collision(self, group, other):
