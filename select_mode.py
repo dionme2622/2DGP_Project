14deabb0f56240b976caf06788a33c223905c770
hyunholee <dionme2622@gmail.com>
2023-11-28 12:11:40 +0900

    시작 버튼 누를 시 화면 애니메이션 추가

--- select_mode.py
+++ select_mode.py
@@ -1,4 +1,4 @@
-from pico2d import load_image, get_events, clear_canvas, update_canvas
+from pico2d import load_image, get_events, clear_canvas, update_canvas, get_time
 from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE, SDLK_SPACE, SDL_MOUSEBUTTONDOWN, SDL_BUTTON_LEFT
 import game_framework, game_world, play_mode
 
@@ -8,10 +8,19 @@ import help_mode
 
 root = Tk()
 WIDTH, HEIGHT = root.winfo_screenwidth(), root.winfo_screenheight()
-
+PI = 3.141592
 def init():
-    global image
+    global image, background, word
+    global size_W, size_H, sradian, sangle
+    global start, wevent, time
+    size_W = 1980
+    size_H = 1080
+    sradian, sangle = 0, 0
+    wevent, start = False, False
+    time = 0.0
     image = load_image('./object/Select.png')
+    background = load_image('./object/background.png')
+    word = load_image('./object/word.png')
     pass
 
 
@@ -23,6 +32,7 @@ def finish():
 def handle_events():
     global player1
     global player2
+    global start, wevent, time
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -31,7 +41,8 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
             if event.x >= 320 and event.x <= 560 and event.y >= 980 and event.y <= 1060:
-                game_framework.change_mode(play_mode)
+                wevent = True
+
             elif event.x >= 660 and event.x <= 900 and event.y >= 980 and event.y <= 1060:
                 game_framework.change_mode(help_mode)
             elif event.x >= 1020 and event.x <= 1260 and event.y >= 980 and event.y <= 1060:
@@ -42,12 +53,34 @@ def handle_events():
 
 
 def update():
+    global start, wevent, time
+    if wevent == True:
+        time = get_time()
+    print(time)
+    if time > 3.0:
+        start = True
+        wevent = False
+    if start == True:
+        screen_event()
     pass
 
 
+def screen_event():
+    global size_W, size_H, sradian, sangle
+    size_W -= 1.8 * 8
+    size_H -= 1 * 8
+    sradian += 3
+    sangle = sradian * PI / 180
+    if size_W < 0 and size_H < 0:
+        game_framework.change_mode(play_mode)
 def draw():
     clear_canvas()
-    image.clip_draw(0, 0, 2702, 1542, WIDTH//2, HEIGHT//2, WIDTH, HEIGHT)
+    background.draw(WIDTH//2, HEIGHT//2)
+    image.clip_composite_draw(0, 0, 2702, 1542, sangle, ' ', WIDTH//2, HEIGHT//2, size_W, size_H)
+    if wevent == True:
+        word.clip_draw(0, 0, 835, 173, 950, 370, 1245, 200)
+        # 3초 후 event = false 그리고 start = true로 만들기
+
     update_canvas()
     pass
 
