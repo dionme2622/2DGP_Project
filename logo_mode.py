c7d29c054d22e149c90bbc709f6ba9e55a4d2073
hyunholee <dionme2622@gmail.com>
2023-12-02 17:47:16 +0900

    5초가 지나면 title 모드로 변경

--- logo_mode.py
+++ logo_mode.py
@@ -1,13 +1,16 @@
 from pico2d import load_image, get_events, clear_canvas, update_canvas, get_time
-
+from pico2d import *
 import game_framework
 import title_mode
+from tkinter import *
 
+root = Tk()
+WIDTH, HEIGHT = root.winfo_screenwidth(), root.winfo_screenheight()
 
 def init():
     global image
     global logo_start_time
-    image = load_image('tuk_credit.png')
+    image = load_image('./object/tuk_credit.png')
     logo_start_time = get_time()
     pass
 
@@ -23,14 +26,17 @@ def handle_events():
 
 def update():
     global running
-    if get_time() - logo_start_time >= 2.0:
+
+    if get_time() - logo_start_time >= 5.0:
         game_framework.change_mode(title_mode)
     pass
 
 
 def draw():
     clear_canvas()
-    image.draw(400, 300)
+
+    image.clip_draw(0, 0, 800, 600, WIDTH // 2, HEIGHT // 2, WIDTH, HEIGHT)
+
     update_canvas()
     pass
 
