2558e553c21bbd6b075f51078976c649a59cdb70
hyunholee <dionme2622@gmail.com>
2023-12-02 18:06:27 +0900

    타이틀 모드 생성 완료

--- title_mode.py
+++ title_mode.py
@@ -1,13 +1,21 @@
 from pico2d import load_image, get_events, clear_canvas, update_canvas, get_time
-from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE, SDLK_SPACE
+from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE, SDLK_SPACE, SDLK_z
 
 import game_framework
+import game_world
 import play_mode
+from tkinter import *
+from title import Title
+import select_mode
 
+root = Tk()
+WIDTH, HEIGHT = root.winfo_screenwidth(), root.winfo_screenheight()
 
 def init():
-    global image
-    image = load_image('title.png')
+    global title
+    title = Title()
+
+    game_world.add_object(title, 0)
     pass
 
 
@@ -23,7 +31,9 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
-            game_framework.change_mode(play_mode)
+            game_framework.change_mode(select_mode)
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_z:
+            game_framework.change_mode(select_mode)
     pass
 
 
@@ -33,7 +43,7 @@ def update():
 
 def draw():
     clear_canvas()
-    image.draw(400, 300)
+    game_world.render()
     update_canvas()
     pass
 
