bb5facf51e87c17460f7b966b2c4d2c932713763
hyunholee <dionme2622@gmail.com>
2023-12-02 19:52:04 +0900

    logo가 생성될때 알파값 적용

--- logo_mode.py
+++ logo_mode.py
@@ -6,12 +6,17 @@ from tkinter import *
 
 root = Tk()
 WIDTH, HEIGHT = root.winfo_screenwidth(), root.winfo_screenheight()
-
+alpha = 0.0
+alpha_increase = 0.005
 def init():
-    global image
+    global image, background
     global logo_start_time
+    global alpha
+    background = load_image('./object/logo_mode_background.png')
     image = load_image('./object/tuk_credit.png')
     logo_start_time = get_time()
+
+
     pass
 
 
@@ -20,13 +25,14 @@ def finish():
 
 
 def handle_events():
-    events = get_events()
     pass
 
 
 def update():
-    global running
-
+    global alpha, alpha_increase
+    if alpha < 1.0:
+        alpha += alpha_increase
+    image.opacify(alpha)
     if get_time() - logo_start_time >= 5.0:
         game_framework.change_mode(title_mode)
     pass
@@ -34,9 +40,8 @@ def update():
 
 def draw():
     clear_canvas()
-
-    image.clip_draw(0, 0, 800, 600, WIDTH // 2, HEIGHT // 2, WIDTH, HEIGHT)
-
+    background.draw(WIDTH // 2, HEIGHT // 2)
+    image.clip_draw(0, 0, 800, 600, WIDTH // 2, HEIGHT // 2, 2000, 1500)
     update_canvas()
     pass
 
