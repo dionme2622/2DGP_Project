8d1451430810efd8e965101fe950a96e90c6408c
hyunholee <dionme2622@gmail.com>
2023-12-03 14:25:51 +0900

    option mode 구현 완료

--- option_mode.py
+++ option_mode.py
@@ -1,4 +1,4 @@
-from pico2d import load_image, get_events, clear_canvas, update_canvas
+from pico2d import *
 from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE, SDLK_SPACE, SDL_MOUSEBUTTONDOWN, SDL_BUTTON_LEFT
 import game_framework, game_world, play_mode
 
@@ -8,10 +8,13 @@ import select_mode
 
 root = Tk()
 WIDTH, HEIGHT = root.winfo_screenwidth(), root.winfo_screenheight()
+import tkinter as tk
+from tkinter import ttk
 
 def init():
-    global image
-    image = load_image('./object/help.png')
+    global sound, image
+    image = load_image('./object/option.png')
+    sound = 40
     pass
 
 
@@ -28,8 +31,13 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
         elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
-            if event.x >= 1700 and event.x <= 1920 and event.y >= 945 and event.y <= 1045:
+            if event.x >= 590 and event.x <= 845 and event.y >= 670 and event.y <= 750:
+                select_mode.volume = 40
                 game_framework.pop_mode()
+            elif event.x >= 1110 and event.x <= 1350 and event.y >= 670 and event.y <= 750:
+                select_mode.volume = 0
+                game_framework.pop_mode()
+
 
 def update():
     pass
@@ -37,7 +45,7 @@ def update():
 
 def draw():
     clear_canvas()
-    image.clip_draw(0, 0, 1375, 640, WIDTH//2, HEIGHT//2, WIDTH, HEIGHT)
+    image.clip_draw(0, 0, 1980, 1080, WIDTH//2, HEIGHT//2, WIDTH, HEIGHT)
     update_canvas()
     pass
 
