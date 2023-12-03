d42384ed60512fa50e8553f433a97aba5582154e
hyunholee <dionme2622@gmail.com>
2023-12-03 18:13:06 +0900

    스킬 쿨타임 표시

--- background.py
+++ background.py
@@ -1,7 +1,9 @@
-from pico2d import load_image, load_music
+from pico2d import load_image, load_music, load_font, get_time
 from tkinter import *
 
 import select_mode
+from blueteam import Blueteam
+from redteam import Redteam
 
 root = Tk()
 WIDTH, HEIGHT = root.winfo_screenwidth(), root.winfo_screenheight()
@@ -36,10 +38,19 @@ class Heart:
 class Icon:
     def __init__(self):
         self.image = load_image('./object/skill_icon.png')
-
+        self.font = load_font('./object/ENCR10B.TTF', 50)
     def draw(self):
         self.image.clip_draw(0, 0, 45, 45, 400, 930, 90, 90)
         self.image.clip_draw(0, 0, 45, 45, 1580, 930, 90, 90)
-
+        if float(Blueteam.skill_wait_time) + 30 - float(get_time()) < 0:
+            self.font.draw(500, 930, f'ON',(255, 255, 255))
+        else:
+            self.font.draw(500, 930, f'{float(Blueteam.skill_wait_time) + 30 - float(get_time()):.1f}',
+                  (255, 255, 255))
+        if float(Redteam.skill_wait_time) + 30 - float(get_time()) < 0:
+            self.font.draw(1380, 930, f'ON', (255, 255, 255))
+        else:
+            self.font.draw(1380, 930, f'{float(Redteam.skill_wait_time) + 30 - float(get_time()):.1f}',
+                  (255, 255, 255))
     def update(self):
         pass
