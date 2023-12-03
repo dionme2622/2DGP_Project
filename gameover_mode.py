601b2067a4f78830249def315a6c209563f97f72
hyunholee <dionme2622@gmail.com>
2023-12-03 17:51:05 +0900

    게임오버 씬 구현 완료

--- gameover_mode.py
+++ gameover_mode.py
@@ -1,6 +1,10 @@
-from pico2d import load_image, get_events, clear_canvas, update_canvas, get_time
-from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE, SDLK_SPACE, SDLK_z, SDLK_RETURN
+import time
 
+from pico2d import load_image, get_events, clear_canvas, update_canvas, get_time, load_font, load_music
+from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE, SDLK_SPACE, SDLK_z, SDLK_RETURN, SDL_MOUSEBUTTONDOWN, \
+    SDL_BUTTON_LEFT
+
+import blueteam
 import game_framework
 import game_world
 import play_mode
@@ -10,14 +14,73 @@ import select_mode
 
 root = Tk()
 WIDTH, HEIGHT = root.winfo_screenwidth(), root.winfo_screenheight()
-alpha = 0.0
-alpha_increase = 0.01
-def init():
-    global title, word
-    title = Title()
-    word = load_image('./object/press.png')
+FRAMES_PER_ACTION = 1
+TIME_PER_ACTION = 1.0
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+class BackGround:
+    image = None
+    sound = None
+    def __init__(self):
+        if BackGround.image == None:
+            BackGround.image = load_image('./object/select_box.png')
+        if BackGround.sound == None:
+            BackGround.sound = load_music('./bgm/mus_gameover.ogg')
+        BackGround.sound.set_volume(40)
+        BackGround.sound.play()
+    def draw(self):
+        self.image.clip_draw(0, 0, WIDTH, HEIGHT, WIDTH // 2, HEIGHT // 2, WIDTH, HEIGHT)
 
-    game_world.add_object(title, 0)
+    def update(self):
+        pass
+    pass
+
+
+class Heart:
+    image = None
+    def __init__(self):
+        if Heart.image == None:
+            Heart.image = load_image('./character/heart.png')
+        self.heart_break = False
+        self.alpha = 1.0
+        self.font = load_font('./object/ENCR10B.TTF', 50)
+        self.time = 0.0
+    def draw(self):
+        if not self.heart_break:
+            if blueteam.survivor == 0:
+                self.image.clip_draw(0, 16, 16, 16, 990, 500, 32, 32)
+            else:
+                self.image.clip_draw(16, 16, 16, 16, 990, 500, 32, 32)
+        else:
+            if blueteam.survivor == 0:
+                self.image.clip_draw(0, 0, 20, 16, 990, 500, 32, 32)
+                if self.time > 3.0:
+                    self.alpha = 1.0
+                    self.image.clip_draw(16, 16, 16, 16, 990, 500, 64, 64)
+                    self.font.draw(950, 600, f'WIN', (255, 255, 255))
+            else:
+                self.image.clip_draw(20, 0, 20, 16, 990, 500, 32, 32)
+                if self.time > 3.0:
+                    self.alpha = 1.0
+                    self.image.clip_draw(0, 16, 16, 16, 990, 500, 64, 64)
+                    self.font.draw(950, 600, f'WIN', (255, 255, 255))
+    def update(self):
+        self.time = (self.time + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time)
+        if self.time > 2.0:
+            self.heart_break = True
+            Heart.image.opacify(self.alpha)
+            if self.alpha > 0:
+                self.alpha -= 0.01
+        pass
+
+    pass
+def init():
+    global background, heart, font
+
+
+    background = BackGround()
+    game_world.add_object(background, 0)
+    heart = Heart()
+    game_world.add_object(heart, 1)
     pass
 
 
@@ -32,29 +95,20 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_RETURN:
-            game_framework.change_mode(select_mode)
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_z:
-            game_framework.change_mode(select_mode)
+        elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
+            if event.x >= 1680 and event.x <= 1920 and event.y >= 25 and event.y <= 115:
+                game_framework.change_mode(select_mode)
     pass
 
 
 def update():
-    global alpha, alpha_increase
-    if alpha > 1.0:
-        alpha_increase *= -1
-    elif alpha < 0.0:
-        alpha_increase *= -1
-    alpha += alpha_increase
-    word.opacify(alpha)
+    game_world.update()
     pass
 
 
 def draw():
     clear_canvas()
     game_world.render()
-    word.clip_draw(0, 0, 402, 38, 702 + 220, 260)
-
     update_canvas()
     pass
 
