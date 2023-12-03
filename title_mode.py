926f21da94b0acb1084231e32602a2a4973d6286
hyunholee <dionme2622@gmail.com>
2023-12-03 13:43:58 +0900

    sound 설정완료

--- title_mode.py
+++ title_mode.py
@@ -1,5 +1,5 @@
 from pico2d import load_image, get_events, clear_canvas, update_canvas, get_time
-from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE, SDLK_SPACE, SDLK_z
+from sdl2 import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE, SDLK_SPACE, SDLK_z, SDLK_RETURN
 
 import game_framework
 import game_world
@@ -10,10 +10,12 @@ import select_mode
 
 root = Tk()
 WIDTH, HEIGHT = root.winfo_screenwidth(), root.winfo_screenheight()
-
+alpha = 0.0
+alpha_increase = 0.01
 def init():
-    global title
+    global title, word
     title = Title()
+    word = load_image('./object/press.png')
 
     game_world.add_object(title, 0)
     pass
@@ -30,7 +32,7 @@ def handle_events():
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_RETURN:
             game_framework.change_mode(select_mode)
         elif event.type == SDL_KEYDOWN and event.key == SDLK_z:
             game_framework.change_mode(select_mode)
@@ -38,12 +40,21 @@ def handle_events():
 
 
 def update():
+    global alpha, alpha_increase
+    if alpha > 1.0:
+        alpha_increase *= -1
+    elif alpha < 0.0:
+        alpha_increase *= -1
+    alpha += alpha_increase
+    word.opacify(alpha)
     pass
 
 
 def draw():
     clear_canvas()
     game_world.render()
+    word.clip_draw(0, 0, 402, 38, 702 + 220, 260)
+
     update_canvas()
     pass
 
