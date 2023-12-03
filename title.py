3dde5dfbd8ab251428ae643712744cd03af62ede
hyunholee <dionme2622@gmail.com>
2023-12-03 13:44:02 +0900

    sound 설정완료

--- title.py
+++ title.py
@@ -3,16 +3,16 @@ from tkinter import *
 
 root = Tk()
 WIDTH, HEIGHT = root.winfo_screenwidth(), root.winfo_screenheight()
-
-
+alpha = 0.0
 class Title:
-
     def __init__(self):
         self.image = load_image('./object/title.png')
-        self.word = load_image('./object/press.png')
-        self.word.opacify(0)
+        self.bgm = load_music('./bgm/mus_story.ogg')
+        self.bgm.set_volume(32)
+        self.bgm.play()
     def draw(self):
         self.image.clip_draw(0, 0, WIDTH, HEIGHT, WIDTH // 2, HEIGHT // 2, WIDTH, HEIGHT)
-        self.word.clip_draw(0, 0, 402, 38, 702 + 220, 260)
     def update(self):
+        global alpha
+        alpha += 0.1
         pass
